ab7a180fc3a790dec33163e63e5bd7dbdea38701
Terrence <tmitc@users.noreply.github.com>
2022-07-14 17:58:49 -0500

    Initial commit

---
+++ src/TriviaAttack.java
@@ -0,0 +1,31 @@
+/**
+ * @author tmitc
+ * @version 1.0
+ * TriviaAttack Quiz Game Application
+ * Agile Development Principals Practice
+ */
+
+//TODO minimum working skeleton of application (basic command terminal game)
+
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TriviaAttack
+{
+	public static void main(String[] args) throws IOException
+	{
+		// Scanner/File objects to receive input from keyboard and text file.
+		File myFile = new File("GameQuestions.txt");
+		Scanner inputFile = new Scanner(myFile),
+				keyboard = new Scanner(System.in);
+		
+		//TODO add game questions to actual .txt file
+		//TODO add reading in questions from text file
+		
+		//close input files
+		inputFile.close();
+		keyboard.close();
+		
+	}
+}
